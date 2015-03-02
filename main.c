6a58af6eb6534b85d6514e53515f1d616bc25b75
Jet Holloway <pwnworthy@gmail.com>
2015-03-03 07:16:30 +1100

    Added function get_pulseaudio_bus()

--- main.c
+++ main.c
@@ -1,5 +1,59 @@
 #include <stdlib.h>          // for exit()
+#include <stdio.h>
+#include <string.h>
 #include <gio/gio.h>		// for g_dbus_*
+
+GDBusConnection* get_pulseaudio_bus()
+{
+	GError *error = NULL;
+	const char* pulse_server_string;
+	GDBusConnection *answer;
+
+	// Try environment variable
+	pulse_server_string = getenv("PULSE_DBUS_SERVER");
+
+	if ( pulse_server_string == NULL )
+	{
+		// Otherwise, try using DBus
+		GDBusConnection *connection = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, &error);
+
+		error = NULL;
+		GDBusProxy *proxy = g_dbus_proxy_new_sync( connection,
+		                                           G_DBUS_PROXY_FLAGS_NONE,
+		                                           NULL,  // DBus interface
+		                                           "org.PulseAudio1",   // Name
+		                                           "/org/pulseaudio/server_lookup1",  // path
+		                                           "org.PulseAudio1.ServerLookup1",   // interface
+		                                           NULL,
+		                                           &error );
+
+		GVariant* gvp = g_dbus_proxy_get_cached_property( proxy,
+		                                                  "Address" );
+
+		pulse_server_string = g_variant_get_string(gvp, NULL);
+
+		g_dbus_connection_close_sync(connection, NULL, &error );
+	}
+
+	if ( pulse_server_string == NULL )
+	{
+		g_printerr("Unable to find PulseAudio bus name\n");
+		exit(1);
+	}
+
+	printf("Pulseaudio bus: %s\n", pulse_server_string);
+
+	// Connect to the bus
+	answer = g_dbus_connection_new_for_address_sync( pulse_server_string,  // Address
+	                                        G_DBUS_CONNECTION_FLAGS_NONE,
+	                                        NULL,  // GDBusAuthObserver
+	                                        NULL,  // GCancellable
+	                                        &error );
+
+	error = NULL;
+
+	return answer;
+}
 
 int main()
 {
